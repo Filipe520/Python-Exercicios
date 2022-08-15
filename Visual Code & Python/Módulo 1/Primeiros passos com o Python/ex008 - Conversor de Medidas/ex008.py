156eccad2e0a0a3f5c6e1a58a8d2614f44947f7f
Filipe520 <filipealves520@gmail.com>
2022-08-15 12:44:12 -0300

    atualizando pasta dos exercícios

---
+++ Visual Code & Python/Módulo 1/Primeiros passos com o Python/ex008 - Conversor de Medidas/ex008.py
@@ -0,0 +1,9 @@
+metros = float(input('Uma distância em metros: '))
+km = metros / 1000
+hm = metros / 100
+dam = metros / 10
+dm = metros * 10
+cm = metros * 100
+mm = metros * 1000
+print(f'A medida de {metros}m corresponde a')
+print(f'{km}km  \n{hm}hm  \n{dam}dam   \n{dm}dm   \n{cm}cm  \n{mm}mm')
