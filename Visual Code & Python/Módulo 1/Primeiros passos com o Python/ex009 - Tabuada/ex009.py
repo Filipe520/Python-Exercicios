eea741d6c454409ce3cbd030d7c44306569938a3
Filipe520 <filipealves520@gmail.com>
2022-08-16 07:15:51 -0300

    Adicionando exercicios 9 e 10

--- Visual Code & Python/Módulo 1/Primeiros passos com o Python/ex009 - Tabuada/ex009.py
+++ Visual Code & Python/Módulo 1/Primeiros passos com o Python/ex009 - Tabuada/ex009.py
@@ -1,13 +1,13 @@
 tabuada = int(input('Digite um número para ver sua tabuada: '))
 print('-' * 20)
-#print(f"""{tabuada} x 1 = {tabuada*1}
-#{tabuada} x 2 = {tabuada*2}
-#{tabuada} x 3 = {tabuada*3}
-#{tabuada} x 4 = {tabuada*4}
-#{tabuada} x 5 = {tabuada*5}
-#{tabuada} x 7 = {tabuada*7}
-#{tabuada} x 8 = {tabuada*8}
-#{tabuada} x 9 = {tabuada*9}
-#{tabuada} x 10 = {tabuada*10}
-#""")
+print(f"""{tabuada} x 1 = {tabuada*1}\n
+{tabuada} x 2 = {tabuada*2}\n
+{tabuada} x 3 = {tabuada*3}\n
+{tabuada} x 4 = {tabuada*4}\n
+{tabuada} x 5 = {tabuada*5}\n
+{tabuada} x 7 = {tabuada*7}\n
+{tabuada} x 8 = {tabuada*8}\n
+{tabuada} x 9 = {tabuada*9}\n
+{tabuada} x 10 = {tabuada*10}
+""")
 print('-' * 20)
---
+++ Visual Code & Python/Módulo 1/Primeiros passos com o Python/ex010 - Converso de Moedas/ex010.py
@@ -0,0 +1,6 @@
+print('-=' * 20)
+carteira = float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar = carteira / 5.10
+
+print(f'Com R${carteira} você pode comprar US${dolar:.3}')
+print('-=' * 20)
